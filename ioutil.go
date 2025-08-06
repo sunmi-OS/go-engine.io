a12632d0470194984e5549100c918f7436a44709
wanggui.yang <wanggui.yang@sunmi.com>
2025-08-06 15:20:25 +0800

    fix: send on closed channel panic

--- ioutil.go
+++ ioutil.go
@@ -23,7 +23,17 @@ func (r *connReader) Close() error {
 	if r.closeChan == nil {
 		return nil
 	}
-	r.closeChan <- struct{}{}
+
+	// 使用select来安全地发送数据，避免向已关闭的channel发送
+	select {
+	case r.closeChan <- struct{}{}:
+		// 成功发送
+	case <-r.closeChan:
+		// channel已关闭，不需要发送
+	default:
+		// channel已满或已关闭，忽略
+	}
+
 	r.closeChan = nil
 	return nil
 }
